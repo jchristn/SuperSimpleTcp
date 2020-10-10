ba3bb8ffa0ef7c883d3f4a61ac6864dced99c5f0
Joel Christner <joel.christner@gmail.com>
2020-10-09 20:24:03 -0700

    StartAsync

--- SimpleTcp/SimpleTcpServer.cs
+++ SimpleTcp/SimpleTcpServer.cs
@@ -225,6 +225,25 @@ namespace SimpleTcp
         }
 
         /// <summary>
+        /// Start accepting connections.
+        /// </summary>
+        /// <returns>Task.</returns>
+        public Task StartAsync()
+        {
+            if (_IsListening) throw new InvalidOperationException("SimpleTcpServer is already running.");
+
+            _Listener = new TcpListener(_IPAddress, _Port);
+
+            if (_Keepalive.EnableTcpKeepAlives) EnableKeepalives();
+
+            _Listener.Start();
+            _TokenSource = new CancellationTokenSource();
+            _Token = _TokenSource.Token;
+            _Statistics = new SimpleTcpStatistics();
+            return AcceptConnections(); // sets _IsListening
+        }
+
+        /// <summary>
         /// Stop accepting new connections.
         /// </summary>
         public void Stop()
@@ -457,7 +476,7 @@ namespace SimpleTcp
             } 
         }
 
-        private async void AcceptConnections()
+        private async Task AcceptConnections()
         {
             _IsListening = true;
 
